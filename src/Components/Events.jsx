873a8d38947abe1447df335cd6a96e0fde0dcf4c
faju27 <fajartk27@gmail.com>
2024-10-16 23:55:32 +0530

    timer added

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -2,7 +2,20 @@ import React, { useEffect, useState } from 'react';
 
 const Events = () => {
 
+    const [counter, setCounter] = useState(null)
+    const [count ,setCount] = useState(null)
     const [axis,setAxis] = useState({x : 0 ,y : 0})
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(prev => prev + 1)
+        }, 1000);
+        return () => {
+          clearInterval(interval)  
+        };
+    }, [count]);
+
+
 
     const handleMouse = (i) => {
         setAxis({x : i.clientX, y : i.clientY})
@@ -20,6 +33,11 @@ const Events = () => {
     
     return (
     <div className='text-success bg-warning' style={{height:'100vh'}} >
+        <div className='d-flex flex-column align-items-center'>
+            <div> {counter} </div>
+            <div> {count} </div>
+            <div><button className='btn btn-danger' onClick={() => setCount(count + 1)}>+</button></div>
+        </div>
        <div className='bg-danger position-absolute rounded-5 ' style={{height:'20px',width:'20px',top:`${axis.y}px`,left:`${axis.x}px`,filter:'blur(1px)'}}></div>
     </div>
     );
